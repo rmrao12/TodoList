19699fb9ef57c129a1d7635a9b23d7e5a26803af
rmrao12 <ridarao.rr@gmail.com>
2024-08-08 11:57:27 +0500

    first commit

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import TodoItem from "./TodoItem";
+
+function Todo() {
+  const [todosList, setTodos] = useState([]);
+  const [todoValue, setValue] = useState("");
+
+const handleValue=(e)=>{
+    setValue(e.target.value)
+}
+
+  const handleSubmit = () => {
+   
+    if (!todoValue) return;
+    addTodo(todoValue);
+    setValue("");
+  };
+
+  const addTodo = (text) => {
+    const newTodos = [...todosList, { text, isEditing: false }];
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = [...todosList];
+    console.log(newTodos)
+   
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+  console.log(todosList)
+
+  const toggleEditTodo = (index) => {
+    
+    const newTodos = [...todosList];
+    
+   
+    for (let i = 0; i < newTodos.length; i++) {
+      if (i === index) {
+      
+        newTodos[i] = { ...newTodos[i], isEditing: !newTodos[i].isEditing };
+      }
+    }
+  
+   
+    setTodos(newTodos);
+  };
+
+  const editTodo = (index, newText) => {
+   
+    const newTodos = [...todosList];
+    
+    
+    for (let i = 0; i < newTodos.length; i++) {
+      if (i === index) {
+       
+        newTodos[i] = { ...newTodos[i], text: newText, isEditing: false };
+      }
+    }
+  
+   
+    setTodos(newTodos);
+  };
+  
+  const todoItems = [];
+  for (let i = 0; i < todosList.length; i++) {
+    todoItems.push(
+      <TodoItem key={i} index={i} todo={todosList[i]} deleteTodo={deleteTodo} toggleEditTodo={toggleEditTodo} editTodo={editTodo}/>
+    );
+  }
+
+  return (
+    <div className="md:max-w-[500px] max-w-[300px] mt-10 my-0 mx-auto p-[20px] border-[1px] border-slate-400 rounded-md shadow-gray-600 bg-[#f9f9f9]">
+      <p className="text-[30px] font-bold text-center">Todo List</p>
+      <div className="flex justify-between p-[8px] mt-[5px]"> 
+        <input type="text" className=" w-[80%] p-[10px] mb-[10px] border-[1px]  border-slate-400 rounded-md" value={todoValue} onChange={handleValue} 
+        placeholder="Add a new todo"/>
+       
+        <button type="submit" onClick={handleSubmit} class="text-gray-900 bg-white border border-gray-300 focus:outline-none
+         hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg 
+         text-sm px-5 py-1.5 me-2 h-12 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700
+          dark:hover:border-gray-600 dark:focus:ring-gray-700">
+        Add</button>
+      
+        </div>
+
+      <ul className="list-none p-0">
+        {todoItems}
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
---
+++ src/Components/TodoItem.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+function TodoItem({ todo, index, deleteTodo, toggleEditTodo, editTodo }) {
+  const [editText, setEditText] = useState(todo.text);
+
+  const handleEdit = () => {
+    toggleEditTodo(index);
+  };
+
+  const handleSave = () => {
+    editTodo(index, editText);
+  };
+
+  return (
+    <li className="flex justify-between p-[8px] mt-[5px] bg-[#fff] border-[1px] border-[#ddd] rounded-md">
+      {todo.isEditing ? (
+        <>
+          <input type="text" value={editText} className=" w-[80%] border-[1px]  border-slate-400 rounded-md"
+          onChange={(e) => setEditText(e.target.value)}/>            
+          <button onClick={handleSave}
+          class="text-gray-900 bg-white border border-gray-300 focus:outline-none
+          hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg 
+          text-sm px-5 py-1.5 me-2 p  dark:bg-gray-800 dark:text-white dark:border-gray-600
+           dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+         Save</button>
+        </>
+      ) : (
+        <>
+          <span className="  flex-1 content-center">{todo.text}</span>
+          <button onClick={handleEdit} type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none
+           hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg 
+           text-sm px-5 py-1.5 me-2  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+          Edit</button>
+          <button onClick={() => deleteTodo(index)}type="button" class="text-gray-900
+           bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 
+           focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-1.5 
+           me-2  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+          Delete</button>
+        </>
+      )}
+    </li>
+  );
+}
+
+export default TodoItem;
